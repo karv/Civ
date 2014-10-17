284ef77fdaa6d38b54e4db935100509e86e890c2
karv <karvayoEdgar@gmail.com>
2014-10-17 15:53:11 -0500

    Elimiado duplicado de UnidadRAW

--- CivLibrary/Unidad/Unidad.cs
+++ CivLibrary/Unidad/Unidad.cs
@@ -6,45 +6,6 @@ using System.Collections.Generic;
 namespace Civ
 {
     /// <summary>
-    /// Representa una clase de unidad
-    /// </summary>
-    public class UnidadRAW
-    {
-        /// <summary>
-        /// El nombre de la clase de unidad.
-        /// </summary>
-        public string Nombre;
-
-        /// <summary>
-        /// Lista de modificadores de combate de la unidad.
-        /// </summary>
-        public ListaPeso<string> Mods;
-
-        /// <summary>
-        /// Fuerza de la unidad.
-        /// </summary>
-        public float Fuerza;
-
-        /// <summary>
-        /// Flags.
-        /// </summary>
-        public List<string> Flags;
-
-            // Reqs
-        /// <summary>
-        /// Requerimientos para crearse.
-        /// </summary>
-        public List<IRequerimiento> Reqs;
-
-        /// <summary>
-        /// Población productiva que requiere para entrenar.
-        /// </summary>
-        public ulong CostePoblación;
-
-
-    }
-
-    /// <summary>
     /// Representa a una instancia de unidad.
     /// </summary>
     public class Unidad
