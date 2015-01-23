89d6e6fb0f39836c5a6335255b28cd2f9d302038
karv <karvayoEdgar@gmail.com>
2015-01-22 19:16:07 -0600

    Referencias corregidas para funcionar con DataContract

--- Store/Store/Store/Store2.cs
+++ Store/Store/Store/Store2.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
-//using System.Linq;
 using System.Text;
-//using System.Threading.Tasks;
-//using XML;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Runtime.Serialization;
+
 using System.IO;
 
 namespace Store
@@ -11,27 +12,42 @@ namespace Store
 
 	public static partial class Store<T>
 	{
-		public static void Serialize(string Filename, T Data)
+        /// <summary>
+        /// Serializa un objeto a un archivo xml
+        /// </summary>
+        /// <param name="xml">Archivo en dónde guardar el xml</param>
+        /// <param name="Data">Datos a guardar.</param>
+		public static void Serialize(string xml, T Data)
 		{
-			System.Xml.Serialization.XmlSerializer writer =
-				new System.Xml.Serialization.XmlSerializer(typeof(T));
-			System.IO.StreamWriter file =
-				new System.IO.StreamWriter(Filename, false, Encoding.UTF8);
 
-			writer.Serialize(file, Data);
-			file.Flush ();
-			file.Close ();
+            //using(FileStream memoryStream = new FileStream(xml, FileMode.Create))
+            //using(StreamWriter reader = new StreamWriter(memoryStream)) {
+            var settings = new XmlWriterSettings { Indent = true, IndentChars = "\t" };
+            XmlWriter wr = XmlWriter.Create(xml, settings);
+            //wr.Settings = settings;
+            DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+            serializer.WriteObject(wr, Data);
+            wr.Close();
 		}
 
-		public static T Deserialize (string Filename)
+        /// <summary>
+        /// Deserializa un objeto
+        /// </summary>
+        /// <param name="xml">nombre del archivo de dónde deserealizar.</param>
+        /// <returns>Devuelve un objeto con la información deserializada.</returns>
+		public static T Deserialize (string xml)
 		{
-			T ret;
-			System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(T));
-			System.IO.StreamReader file = new StreamReader(Filename);
 
-			ret = (T)reader.Deserialize(file);
-			file.Close();
-			return ret;
+            using (Stream stream = new MemoryStream())
+            {
+                //byte[] data = System.Text.Encoding.UTF8.GetBytes (xml);
+                //stream.Write (data, 0, data.Length);
+                //stream.Position = 0;
+                XmlTextReader xr = new XmlTextReader(new FileStream(xml, FileMode.Open));
+                DataContractSerializer deserializer = new DataContractSerializer(typeof(T));
+                object r = deserializer.ReadObject(xr);
+                return (T)r;
+            }
 		}
 	}
 }
