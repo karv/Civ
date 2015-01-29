9f0f2983c402000bc1a452b9ae6973fa48b7e84f
karv <karvayoEdgar@gmail.com>
2015-01-28 21:02:05 -0600

    Formato

--- CivWin/FrmCiudad.cs
+++ CivWin/FrmCiudad.cs
@@ -12,112 +12,112 @@ using CivWin;
 
 namespace CivWin
 {
-    public partial class FrmCiudad : Form, IDibujable
-    {
-        /// <summary>
-        /// Devuelve la ciudad vinculada a este formulario.
-        /// </summary>
-        public readonly Ciudad ciudad;
-        /// <summary>
-        /// Crea una instancia de este formulario.
-        /// </summary>
-        /// <param name="C">La ciudad vinculada a este formulario.</param>
-        public FrmCiudad(Ciudad C)
-        {
-            ciudad = C;
-            InitializeComponent();
+	public partial class FrmCiudad : Form, IDibujable
+	{
+		/// <summary>
+		/// Devuelve la ciudad vinculada a este formulario.
+		/// </summary>
+		public readonly Ciudad ciudad;
+		/// <summary>
+		/// Crea una instancia de este formulario.
+		/// </summary>
+		/// <param name="C">La ciudad vinculada a este formulario.</param>
+		public FrmCiudad(Ciudad C)
+		{
+			ciudad = C;
+			InitializeComponent();
 
-            Draw();
-        }
+			Draw();
+		}
 
-        public void Draw()
-        {
-            Text = ciudad.Nombre;
+		public void Draw()
+		{
+			Text = ciudad.Nombre;
 
-            // Recursos
-            listRecursos.Items.Clear();
-            foreach (var x in ciudad.Almacén.Keys)
-            {
-                listRecursos.Items.Add(string.Format("{0} - {1}", x, ciudad.Almacén[x]));
-            }
+			// Recursos
+			listRecursos.Items.Clear();
+			foreach (var x in ciudad.Almacén.Keys)
+			{
+				listRecursos.Items.Add(string.Format("{0} - {1}", x, ciudad.Almacén[x]));
+			}
 
-            // textInfo            
-            List<string> strInfo = new List<string>();
-            strInfo.Add("Población: " + ciudad.getPoblación.ToString());
-            strInfo.Add(string.Format("Distribución por edad: {0} / {1} / {2}", ciudad.getPoblaciónPreProductiva, ciudad.PoblaciónProductiva, ciudad.getPoblaciónPostProductiva));
-            textInfo.Lines = strInfo.ToArray();
+			// textInfo            
+			List<string> strInfo = new List<string>();
+			strInfo.Add("Población: " + ciudad.getPoblación.ToString());
+			strInfo.Add(string.Format("Distribución por edad: {0} / {1} / {2}", ciudad.getPoblaciónPreProductiva, ciudad.PoblaciónProductiva, ciudad.getPoblaciónPostProductiva));
+			textInfo.Lines = strInfo.ToArray();
 
-            // Edificios
-            listEdificios.Items.Clear();
-            foreach (var x in ciudad.Edificios)
-            {
-                listEdificios.Items.Add(x.Nombre);
-            }
+			// Edificios
+			listEdificios.Items.Clear();
+			foreach (var x in ciudad.Edificios)
+			{
+				listEdificios.Items.Add(x.Nombre);
+			}
 
-            // Trabajos y trabajadores
-            listTrabajos.Items.Clear();
-            foreach (var x in ciudad.ObtenerListaTrabajosRAW)
-            {
-                listTrabajos.Items.Add(x);
-            }
+			// Trabajos y trabajadores
+			listTrabajos.Items.Clear();
+			foreach (var x in ciudad.ObtenerListaTrabajosRAW)
+			{
+				listTrabajos.Items.Add(x);
+			}
 
-            // El "construyendo".
-            comboConstruir.Items.Clear();
-            foreach (var x in ciudad.Construibles())
-            {
-                comboConstruir.Items.Add(x);
-            }
-            comboConstruir.SelectedItem = ciudad.RAWConstruyendo;
+			// El "construyendo".
+			comboConstruir.Items.Clear();
+			foreach (var x in ciudad.Construibles())
+			{
+				comboConstruir.Items.Add(x);
+			}
+			comboConstruir.SelectedItem = ciudad.RAWConstruyendo;
 
-        }
+		}
 
-        /// <summary>
-        /// Devuele el trabajo seleccionado en listTrabajos. Si no existe, lo crea.
-        /// </summary>
-        /// <returns></returns>
-        private Trabajo TrabajoSeleccionado()
-        {
-            TrabajoRAW Selec = (TrabajoRAW)listTrabajos.SelectedItem;
-            Trabajo Sel = ciudad.EncuentraInstanciaTrabajo(Selec);
-            if (Sel == null)    // Si no existen instancia, se crea
-            {
-                Trabajo NTrab = new Trabajo(Selec, ciudad.EncuentraInstanciaEdificio(Selec.Edificio));
-                Sel = NTrab;
-            }            
-            return Sel;
-        }
+		/// <summary>
+		/// Devuele el trabajo seleccionado en listTrabajos. Si no existe, lo crea.
+		/// </summary>
+		/// <returns></returns>
+		private Trabajo TrabajoSeleccionado()
+		{
+			TrabajoRAW Selec = (TrabajoRAW)listTrabajos.SelectedItem;
+			Trabajo Sel = ciudad.EncuentraInstanciaTrabajo(Selec);
+			if (Sel == null)    // Si no existen instancia, se crea
+			{
+				Trabajo NTrab = new Trabajo(Selec, ciudad.EncuentraInstanciaEdificio(Selec.Edificio));
+				Sel = NTrab;
+			}
+			return Sel;
+		}
 
-        private void listTrabajos_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (listTrabajos.SelectedItem != null)
-            {
-                Trabajo Sel = TrabajoSeleccionado();
-                numTrabajador.Minimum = 0;
-                numTrabajador.Maximum = Sel.MaxTrabajadores;
-                numTrabajador.Value = Sel.Trabajadores;
-                numTrabajador.Enabled = true;
-            }
-            else
-            {
-                numTrabajador.Value = 0;
-                numTrabajador.Enabled = false;
-            }
+		private void listTrabajos_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (listTrabajos.SelectedItem != null)
+			{
+				Trabajo Sel = TrabajoSeleccionado();
+				numTrabajador.Minimum = 0;
+				numTrabajador.Maximum = Sel.MaxTrabajadores;
+				numTrabajador.Value = Sel.Trabajadores;
+				numTrabajador.Enabled = true;
+			}
+			else
+			{
+				numTrabajador.Value = 0;
+				numTrabajador.Enabled = false;
+			}
 
-        }
+		}
 
-        private void numTrabajador_ValueChanged(object sender, EventArgs e)
-        {
-            Trabajo T = TrabajoSeleccionado();
-            T.Trabajadores = (ulong)numTrabajador.Value;
-            numTrabajador.Value = T.Trabajadores;
-        }
+		private void numTrabajador_ValueChanged(object sender, EventArgs e)
+		{
+			Trabajo T = TrabajoSeleccionado();
+			T.Trabajadores = (ulong)numTrabajador.Value;
+			numTrabajador.Value = T.Trabajadores;
+		}
 
-        private void comboConstruir_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (comboConstruir.SelectedItem != ciudad.RAWConstruyendo)
-            {
-                ciudad.RAWConstruyendo = (EdificioRAW)comboConstruir.SelectedItem;                
-            }
-        }
-    }
+		private void comboConstruir_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (comboConstruir.SelectedItem != ciudad.RAWConstruyendo)
+			{
+				ciudad.RAWConstruyendo = (EdificioRAW)comboConstruir.SelectedItem;
+			}
+		}
+	}
 }
--- CivWin/Program.cs
+++ CivWin/Program.cs
@@ -11,40 +11,40 @@ using System.Windows.Forms;
 
 namespace CivWin
 {
-    public static class Program
-    {
+	public static class Program
+	{
 
-        private static void DoRead(string f = "Data.xml")
-        {
-            g_.Data = Store.Store<g_Data>.Deserialize(f);
-            
-        }
+		private static void DoRead(string f = "Data.xml")
+		{
+			g_.Data = Store.Store<g_Data>.Deserialize(f);
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
+		}
 
-            DoRead();
-            g_.InicializarJuego();
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
 
-            Civilizacion C = g_.State.Civs[0];
+			DoRead();
+			g_.InicializarJuego();
+
+			Civilizacion C = g_.State.Civs[0];
 			C.getCiudades[0].AlimentoAlmacén = 100;
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCiv(C));
-        }
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new FrmCiv(C));
+		}
 
-        public static void MuestraCiudad(Ciudad C, Form Owner)
-        {
-            FrmCiudad frm = new FrmCiudad(C);
-            frm.Show(Owner);
-           
-        }
-    }
+		public static void MuestraCiudad(Ciudad C, Form Owner)
+		{
+			FrmCiudad frm = new FrmCiudad(C);
+			frm.Show(Owner);
+
+		}
+	}
 }
 
 /// <summary>
@@ -53,5 +53,5 @@ namespace CivWin
 /// </summary>
 public interface IDibujable
 {
-    void Draw();
+	void Draw();
 }
