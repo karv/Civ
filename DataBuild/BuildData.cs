dad74285d71a6441dfd51c9f972c59233cca0f4a
Edgar Carballo <karvayoEdgar@gmail.com>
2016-05-05 18:38:24 -0500

    Agregar este proyecto de construcción

---
+++ DataBuild/BuildData.cs
@@ -0,0 +1,20 @@
+﻿using Civ.RAW;
+using Civ.Global;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DataBuild
+{
+	public static class BuildData
+	{
+		public static void Main ()
+		{
+			var data = new GameData ();
+			var r_Alimento = new Recurso ("Alimento");
+			r_Alimento.Valor = 2;
+			data.Recursos.Add (r_Alimento);
+			data.RecursoAlimento = r_Alimento;
+
+			Store.BinarySerialization.WriteToBinaryFile ("Data.bin", data);
+		}
+	}
+}
