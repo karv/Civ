3cb6e59d4a3b9439210dfb29aa1f9140650f287f
Edgar Carballo <karvayoEdgar@gmail.com>
2016-05-05 19:07:50 -0500

    Agregar algunos datos

--- DataBuild/BuildData.cs
+++ DataBuild/BuildData.cs
@@ -1,6 +1,7 @@
 ﻿using Civ.RAW;
 using Civ.Global;
-using System.Security.Cryptography.X509Certificates;
+using Civ.Ciencias;
+using Civ.Topología;
 
 namespace DataBuild
 {
@@ -9,10 +10,48 @@ namespace DataBuild
 		public static void Main ()
 		{
 			var data = new GameData ();
+
+			#region Recursos
 			var r_Alimento = new Recurso ("Alimento");
 			r_Alimento.Valor = 2;
 			data.Recursos.Add (r_Alimento);
 			data.RecursoAlimento = r_Alimento;
+
+			#region Científicos
+			var r_c_Ciencia = new Recurso ("Ciencia")
+			{
+				Desaparece = true,
+				EsCientifico = true,
+				EsGlobal = true
+			};
+			data.Recursos.Add (r_c_Ciencia);
+
+			#endregion
+
+			#endregion
+
+			#region Ciencia
+			var c_Agricultura = new Ciencia  { Nombre = "Agricultura" };
+			c_Agricultura.Reqs.Recursos [r_c_Ciencia] = 10;
+			data.Ciencias.Add (c_Agricultura);
+			#endregion
+
+			#region Propiedades
+			var p_Alimento = new Propiedad { Nombre = "Alimento" };
+			p_Alimento.Salida.Add (new TasaProdConstante ()
+			{
+				Recurso = r_Alimento,
+				Crecimiento = 1,
+				Max = 1000
+			});
+			#endregion
+			#region Ecosistemas
+			var e_Llanura = new Ecosistema { Nombre = "Llanura" };
+			e_Llanura.Nombres.Add ("Planicie");
+			e_Llanura.PropPropiedad.Add (p_Alimento, 0.8f);
+			data.Ecosistemas.Add (e_Llanura);
+			#endregion
+
 
 			Store.BinarySerialization.WriteToBinaryFile ("Data.bin", data);
 		}
