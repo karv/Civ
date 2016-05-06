e4389016343c3ca643b7fc13b39fd1ba01fd8127
Edgar Carballo <karvayoEdgar@gmail.com>
2016-05-05 19:38:34 -0500

    Actualizar lib Agregar más data La salida binaria de DataBuild va directamente al proj gtk

--- DataBuild/BuildData.cs
+++ DataBuild/BuildData.cs
@@ -17,6 +17,14 @@ namespace DataBuild
 			data.Recursos.Add (r_Alimento);
 			data.RecursoAlimento = r_Alimento;
 
+			#region Construcción
+			var r_Piedra = new Recurso ("Piedra");
+			data.Recursos.Add (r_Piedra);
+
+			var r_Madera = new Recurso ("MAdera");
+			data.Recursos.Add (r_Madera);
+			#endregion
+
 			#region Científicos
 			var r_c_Ciencia = new Recurso ("Ciencia")
 			{
@@ -38,22 +46,77 @@ namespace DataBuild
 
 			#region Propiedades
 			var p_Alimento = new Propiedad { Nombre = "Alimento" };
-			p_Alimento.Salida.Add (new TasaProdConstante ()
+			p_Alimento.Salida.Add (new TasaProdConstante
 			{
 				Recurso = r_Alimento,
 				Crecimiento = 1,
 				Max = 1000
 			});
 			#endregion
+
 			#region Ecosistemas
 			var e_Llanura = new Ecosistema { Nombre = "Llanura" };
 			e_Llanura.Nombres.Add ("Planicie");
 			e_Llanura.PropPropiedad.Add (p_Alimento, 0.8f);
 			data.Ecosistemas.Add (e_Llanura);
+
+			var e_Montañoso = new Ecosistema { Nombre = "Montaña" };
+			e_Montañoso.Nombres.Add ("Montaña");
+			e_Montañoso.Nombres.Add ("Monte");
+			e_Montañoso.Nombres.Add ("Incario");
+			e_Montañoso.PropPropiedad.Add (p_Alimento, 0.3f);
+
+			var e_Desierto = new Ecosistema { Nombre = "Desierto" };
+			e_Desierto.Nombres.Add ("Sahara");
+			e_Desierto.Nombres.Add ("Sahuaro");
+			e_Desierto.PropPropiedad.Add (p_Alimento, 0.2f);
 			#endregion
 
+			#region Edificios
+			var b_Palacio = new EdificioRAW
+			{
+				EsAutoConstruible = true,
+				Nombre = "Palacio",
+				MaxPorCivilizacion = 1,
+				MaxWorkers = 4
+			};
+			b_Palacio.ReqRecursos.Add (r_Madera, 50);
+			b_Palacio.ReqRecursos.Add (r_Piedra, 30);
+			data.Edificios.Add (b_Palacio);
+			#endregion
 
-			Store.BinarySerialization.WriteToBinaryFile ("Data.bin", data);
+			#region Trabajos
+			var t_CienciaPalacio = new TrabajoRAW
+			{
+				Edificio = b_Palacio,
+				Nombre = "Ciencia"
+			};
+			t_CienciaPalacio.SalidaBase [r_c_Ciencia] = 1;
+			data.Trabajos.Add (t_CienciaPalacio);
+
+			#endregion
+
+			#region Unidades
+			var u_LanzaPiedras = new UnidadRAWCombate
+			{
+				Velocidad = 1,
+				Peso = 1,
+				MaxCarga = 0.3f,
+				Defensa = 1,
+				CostePoblación = 1,
+				Dispersión = 0,
+				Nombre = "Lanza piedras",
+				Ataque = 1
+			};
+			u_LanzaPiedras.Flags.Add ("Pie");
+			u_LanzaPiedras.Flags.Add ("Humano");
+			u_LanzaPiedras.Flags.Add ("Rango");
+			u_LanzaPiedras.Reqs [r_Piedra] = 3.5f;
+			data.Unidades.Add (u_LanzaPiedras);
+			#endregion
+
+			Store.BinarySerialization.WriteToBinaryFile (@"../../../gtk/Data.bin", data);
+
 		}
 	}
 }
