0ff15f47bf101f783c833f8a0c6196f9c6dce66f
Edgar Carballo <karvayoEdgar@gmail.com>
2016-05-07 18:11:19 -0500

    Agregar cacería y recolección

--- DataBuild/BuildData.cs
+++ DataBuild/BuildData.cs
@@ -27,6 +27,21 @@ namespace DataBuild
 				EsEcológico = true
 			};
 			data.Recursos.Add (r_Semilla);
+
+			var r_Bestias = new Recurso
+			{
+				Nombre = "Bestias de casa",
+				EsEcológico = true
+			};
+			data.Recursos.Add (r_Bestias);
+
+			var r_Frutas = new Recurso
+			{
+				Nombre = "Frutas",
+				EsEcológico = true
+			};
+			data.Recursos.Add (r_Frutas);
+
 			#region Construcción
 			var r_Martillo = new Recurso
 			{
@@ -56,9 +71,19 @@ namespace DataBuild
 			#endregion
 
 			#region Ciencia
+			var c_Caza = new Ciencia { Nombre = "Caza" };
+			c_Caza.Reqs.Recursos.Add (r_c_Ciencia, 5);
+			data.Ciencias.Add (c_Caza);
+
+			var c_Recolección = new Ciencia { Nombre = "Recolección" };
+			c_Caza.Reqs.Recursos.Add (r_c_Ciencia, 5);
+			data.Ciencias.Add (c_Recolección);
+
 			var c_Agricultura = new Ciencia  { Nombre = "Agricultura" };
 			c_Agricultura.Reqs.Recursos [r_c_Ciencia] = 10;
+			c_Agricultura.Reqs.Ciencias.Add (c_Recolección);
 			data.Ciencias.Add (c_Agricultura);
+
 			#endregion
 
 			#region Propiedades
@@ -69,12 +94,30 @@ namespace DataBuild
 				Crecimiento = 1,
 				Max = 1000
 			});
+
+			var p_Bestias = new Propiedad { Nombre = "Bestias de caza" };
+			p_Bestias.Salida.Add (new TasaProdExp
+			{
+				Recurso = r_Bestias,
+				CrecimientoBase = 3,
+				Max = 5000
+			});
+
+			var p_Frutas = new Propiedad{ Nombre = "Frutas" };
+			p_Frutas.Salida.Add (new TasaProdExp
+			{
+				Recurso = r_Frutas,
+				CrecimientoBase = 1.4f,
+				Max = 4500
+			});
 			#endregion
 
 			#region Ecosistemas
 			var e_Llanura = new Ecosistema { Nombre = "Llanura" };
 			e_Llanura.Nombres.Add ("Planicie");
 			e_Llanura.PropPropiedad.Add (p_Alimento, 0.8f);
+			e_Llanura.PropPropiedad.Add (p_Bestias, 0.7f);
+			e_Llanura.PropPropiedad.Add (p_Frutas, 0.85f);
 			data.Ecosistemas.Add (e_Llanura);
 
 			var e_Montañoso = new Ecosistema { Nombre = "Montaña" };
@@ -82,11 +125,16 @@ namespace DataBuild
 			e_Montañoso.Nombres.Add ("Monte");
 			e_Montañoso.Nombres.Add ("Incario");
 			e_Montañoso.PropPropiedad.Add (p_Alimento, 0.3f);
+			e_Montañoso.PropPropiedad.Add (p_Bestias, 0.4f);
+			e_Montañoso.PropPropiedad.Add (p_Frutas, 0.4f);
+			data.Ecosistemas.Add (e_Montañoso);
 
 			var e_Desierto = new Ecosistema { Nombre = "Desierto" };
 			e_Desierto.Nombres.Add ("Sahara");
 			e_Desierto.Nombres.Add ("Sahuaro");
 			e_Desierto.PropPropiedad.Add (p_Alimento, 0.2f);
+			e_Desierto.PropPropiedad.Add (p_Bestias, 0.2f);
+			data.Ecosistemas.Add (e_Desierto);
 			#endregion
 
 			#region Edificios
@@ -110,6 +158,34 @@ namespace DataBuild
 			};
 			b_Granja.ReqRecursos.Add (r_Martillo, 5);
 			b_Granja.Requiere.Ciencias.Add (c_Agricultura);
+			data.Edificios.Add (b_Granja);
+
+			var b_ReuniónCacería = new EdificioRAW ()
+			{
+				MaxWorkers = 20,
+				Nombre = "Casa de cacería"
+			};
+			b_ReuniónCacería.ReqRecursos.Add (r_Piedra, 10);
+			b_ReuniónCacería.ReqRecursos.Add (r_Madera, 5);
+			b_ReuniónCacería.Requiere.Ciencias.Add (c_Caza);
+			data.Edificios.Add (b_ReuniónCacería);
+
+			var b_RecolRecursos = new EdificioRAW
+			{
+				MaxWorkers = 10,
+				Nombre = "Reunión de recursos",
+				EsAutoConstruible = true
+			};
+			data.Edificios.Add (b_RecolRecursos);
+
+			var b_RecolFrutas = new EdificioRAW
+			{
+				MaxWorkers = 10,
+				Nombre = "Recolección de frutas",
+			};
+			b_RecolFrutas.ReqRecursos.Add (r_Piedra, 2);
+			b_RecolFrutas.ReqRecursos.Add (r_Madera, 1);
+			data.Edificios.Add (b_RecolFrutas);
 			#endregion
 
 			#region Trabajos
@@ -137,6 +213,40 @@ namespace DataBuild
 			};
 			t_SemillasGranja.EntradaBase.Add (r_Alimento, 0.1f);
 			t_SemillasGranja.SalidaBase.Add (r_Semilla, 0.5f);
+			data.Trabajos.Add (t_SemillasGranja);
+
+			var t_Cazar = new TrabajoRAW
+			{
+				Nombre = "Cacería",
+				Edificio = b_ReuniónCacería
+			};
+			t_Cazar.EntradaBase.Add (r_Bestias, 1);
+			t_Cazar.SalidaBase.Add (r_Alimento, 1.2f);
+			data.Trabajos.Add (t_Cazar);
+
+			var t_RecolectarMadera = new TrabajoRAW
+			{
+				Nombre = "Recolectar madera",
+				Edificio = b_RecolRecursos
+			};
+			t_RecolectarMadera.SalidaBase.Add (r_Madera, 2);
+			data.Trabajos.Add (t_RecolectarMadera);
+
+			var t_RecolectarPiedra = new TrabajoRAW
+			{
+				Nombre = "Recolectar piedra",
+				Edificio = b_RecolRecursos
+			};
+			t_RecolectarMadera.SalidaBase.Add (r_Piedra, 1.5f);
+			data.Trabajos.Add (t_RecolectarPiedra);
+
+			var t_RecolectarFruta = new TrabajoRAW
+			{
+				Nombre = "Recolectar fruta",
+				Edificio = b_RecolFrutas
+			};
+			t_RecolectarFruta.SalidaBase.Add (r_Alimento, 2);
+			t_RecolectarFruta.EntradaBase.Add (r_Frutas, 2);
 			#endregion
 
 			#region Unidades
